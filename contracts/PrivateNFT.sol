a3bac971704e456d0166c2fa6fdf5fba660367c3
denissn228 <odxmbpbkrw@rambler.ru>
2024-08-29 00:13:35 +0000

    feat: initiated the project

--- contracts/PrivateNFT.sol
+++ contracts/PrivateNFT.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PrivateNFT is ERC721, ERC721Burnable, Ownable {
     constructor(address initialOwner)
-        ERC721("qwert","qwe")
+        ERC721("qwewrt","wqe")
         Ownable(initialOwner)
     {}
 
